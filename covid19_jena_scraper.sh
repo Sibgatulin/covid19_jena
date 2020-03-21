a9776978664be83c1d1d66e9b5825c7636409e81
Sibgatulin <renat.sibgatulin@gmail.com>
2020-03-21 22:31:25 +0100

    streamlined the interaction of shell and python scripts and added git into play

---
+++ covid19_jena_scraper.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+git pull
+
+JC19_DIR=/home/renat/scripts/covid19_jena
+ENTRY_LAST=$(tail -1 $JC19_DIR/jena_covid19_scraped.csv)
+ENTRY_NEW=$($JC19_DIR/scrape.py)
+if [ "$ENTRY_NEW" != "$ENTRY_LAST" ]; then
+    echo $ENTRY_NEW >> $JC19_DIR/jena_covid19_scraped.csv
+    git add $JC19_DIR/jena_covid19_scraped.csv
+    git commit -m "update from ${ENTRY_NEW%,*}"
+    git push
+fi
