9e679f4e44fde18cc0f41131837b1d57458b6346
Sibgatulin <renat.sibgatulin@gmail.com>
2020-03-26 23:24:22 +0100

    Jena changed the format, and I suppose now the time tag might be absent. Hence I adjusted the regex as well as introduced separate groups for the date and the time (why now from the very beginning?..)

--- scrape.py
+++ scrape.py
@@ -1,10 +1,22 @@
 #!/home/renat/envs/scraper/bin/python
 import re
 import requests
+import dateutil.parser
 from bs4 import BeautifulSoup
 from datetime import datetime
 
 url = "https://gesundheit.jena.de/de/coronavirus"
+
+
+def standardise_format(inp_str, fmt):
+    """ Parses the input string with dateutil and converts to the specified format
+    The rationale for the function is that I do not trust Jena Gesundheitsamt to be
+    be consistent with how they timestamp the data. Yet I sort of rely on it in bigger
+    way in how I scrape the data anyway...
+    """
+    timestamp = dateutil.parser.parse(inp_str)
+    return timestamp.strftime(fmt)
+
 
 if __name__ == "__main__":
 
@@ -18,11 +30,14 @@ if __name__ == "__main__":
 
     # the second and the last ``span`` contains the timestamp
     stand_str = result.find_all("span")[-1].text
-    pattern_stand = re.compile(r".*(?P<timestamp>\d{2}\.\d{2}\.\d{4}, \d{2}:\d{2}).*")
-    timestamp_str = pattern_stand.match(stand_str).group("timestamp")
-    timestamp = datetime.strptime(timestamp_str, "%d.%m.%Y, %H:%M")
-    date = timestamp.strftime("%Y-%m-%d")
-    time = timestamp.strftime("%H:%M")
+    pattern_stand = re.compile(
+        r".*(?P<date>\d{2}\.\d{2}\.\d{4})(, (?P<time>\d{2}:\d{2}))?.*"
+    )
+    timestamp_match = pattern_stand.match(stand_str)
+    date_str = timestamp_match.group("date")
+    time_str = timestamp_match.group("time")
+    date = standardise_format(date_str, "%Y-%m-%d") if date_str else date_str
+    time = standardise_format(time_str, "%Y-%m-%d") if time_str else time_str
 
     # so far they had the number in bold, let's see if I can rely on it
     number_str = result.find("strong").text
